7ad23f80bba27fa3a621eb75bd92997d7e1706c0
sony-level <sony-dilane-level.mbango@isen.yncrea.fr>
2024-06-25 19:48:43 +0200

    feat: ✨ Add migration to create evaluations table

---
+++ db/migrations/ 20230625_create_evaluations_table.sql
@@ -0,0 +1,19 @@
+CREATE TABLE "evaluations" (
+  "evaluation_id" serial PRIMARY KEY,
+  "class_id" int NOT NULL,
+  "title" varchar(100) NOT NULL,
+  "description" text,
+  "max_score" int NOT NULL,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
+);
+
+COMMENT ON TABLE "evaluations" IS 'Stocke les données des évaluations';
+COMMENT ON COLUMN "evaluations"."class_id" IS 'Identifiant de la classe';
+COMMENT ON COLUMN "evaluations"."title" IS 'Titre de l''évaluation';
+COMMENT ON COLUMN "evaluations"."description" IS 'Description de l''évaluation';
+COMMENT ON COLUMN "evaluations"."max_score" IS 'Score maximal';
+COMMENT ON COLUMN "evaluations"."created_at" IS 'Date de création';
+COMMENT ON COLUMN "evaluations"."updated_at" IS 'Date de mise à jour';
+
+ALTER TABLE "evaluations" ADD FOREIGN KEY ("class_id") REFERENCES "classes" ("class_id");
