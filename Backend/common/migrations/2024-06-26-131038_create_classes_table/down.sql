1df2c59f3f949e3b9d32a8b40cf2c40c8841a85f
sony-level <sony-dilane-level.mbango@isen.yncrea.fr>
2024-06-26 15:13:46 +0200

    feat: ✨ add classe table

---
+++ Backend/common/migrations/2024-06-26-131038_create_classes_table/down.sql
@@ -0,0 +1,3 @@
+-- This file should undo anything in `up.sql`
+ DROP TABLE IF EXISTS classes;
+-- Compare this snippet from Bitbox/Backend/common/migrations/2024-06-26-130801_create_users_table/down.sql:
---
+++ Backend/common/migrations/2024-06-26-131038_create_classes_table/up.sql
@@ -0,0 +1,21 @@
+-- Your SQL goes here
+CREATE TABLE IF NOT EXISTS classes (
+    class_id SERIAL PRIMARY KEY,
+    class_name VARCHAR(100) NOT NULL,
+    description TEXT,
+    teacher_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW(),
+    FOREIGN KEY (teacher_id) REFERENCES users(user_id)
+);
+
+CREATE INDEX IF NOT EXISTS classes_teacher_id_idx ON classes (teacher_id);
+CREATE INDEX IF NOT EXISTS classes_class_name_idx ON classes (class_name);
+
+COMMENT ON TABLE classes IS 'Stocke les données des classes';
+COMMENT ON COLUMN classes.class_name IS 'Nom de la classe';
+COMMENT ON COLUMN classes.description IS 'Description de la classe';
+COMMENT ON COLUMN classes.teacher_id IS 'Identifiant du formateur';
+COMMENT ON COLUMN classes.created_at IS 'Date de création';
+COMMENT ON COLUMN classes.updated_at IS 'Date de mise à jour';
+-- Compare this snippet from Bitbox/Backend/common/migrations/2024-06-26-130801_create_users_table/down.sql:
