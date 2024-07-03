43f6ddca18285e6a194438463dcfe3cfb6ae86a0
sony-level <sony-dilane-level.mbango@isen.yncrea.fr>
2024-07-03 23:36:06 +0200

    feat: add function rocket to launch app

---
+++ Backend/user_service/src/main.rs
@@ -0,0 +1,24 @@
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate diesel;
+
+mod handlers;
+mod routes;
+
+
+use common::db::establish_connection;
+
+/**
+ * Lancer le serveur Rocket
+ * @return le serveur Rocket
+ * @throws InternalServerError si la connexion à la base de données ne fonctionne pas
+ * @see establish_connection
+ */
+#[launch]
+fn rocket() -> _ {
+    dotenv::dotenv().ok();
+    let pool = establish_connection();
+    rocket::build()
+        .manage(pool)
+        .mount("/api/v1", routes::routes())
+}
+// Compare this snippet from Bitbox/Backend/user_service/src/handlers.rs:
