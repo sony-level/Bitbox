35f1ec2af440e8adaf84daf1631d35acaf814be8
sony-level <sony-dilane-level.mbango@isen.yncrea.fr>
2024-07-03 21:57:02 +0200

    fix function to ectablished connexion to database

--- Backend/common/src/db.rs
+++ Backend/common/src/db.rs
@@ -1,12 +1,8 @@
 
 extern crate diesel;
 extern crate dotenv;
-
-use diesel::prelude::*;
 use diesel::pg::PgConnection;
 use diesel::r2d2::{self, ConnectionManager};
-use dotenv::dotenv;
-use std::env;
 
 // Alias pour le pool de connexions
 pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
@@ -20,11 +16,10 @@ pub fn init_pool(database_url: &str) -> Pool {
 }
 
 // Établir la connexion à la base de données
-pub fn establish_connection() -> PgConnection {
-    dotenv().ok();
-
-    let database_url = env::var("DATABASE_URL")
-        .expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
+pub fn establish_connection() -> Pool {
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.")
 }
