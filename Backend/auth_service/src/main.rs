15bdd4a9e52f408d7fd8cca1c80d7df2f539d4d0
sony-level <sony-dilane-level.mbango@isen.yncrea.fr>
2024-07-04 04:41:05 +0200

    feat : add main file to launch api service

--- Backend/auth_service/src/main.rs
+++ Backend/auth_service/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+extern crate diesel;
+
+mod handlers;
+mod routes;
+mod models;
+
+use common::db::establish_connection;
+
+/**
+ * Lancer le serveur Rocket
+ * @return le serveur Rocket
+ * @throws InternalServerError si la connexion à la base de données ne fonctionne pas
+ * @see establish_connection
+ */
+#[launch]
+fn rocket() -> _ {
+    dotenv::dotenv().ok();
+    let pool = establish_connection();
+    rocket::build()
+        .manage(pool)
+        .mount("/api/v1", routes::routes())
 }
+// Compare this snippet from Bitbox/Backend/user_service/src/handlers.rs:
