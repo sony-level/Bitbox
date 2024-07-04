e8e9d40901efdc480ff106b730722c0409eeed5e
sony-level <sony-dilane-level.mbango@isen.yncrea.fr>
2024-07-04 04:14:28 +0200

    feat : add instruction to table of db

--- Backend/common/migrations/2024-07-04-011736_registration_links/down.sql
+++ Backend/common/migrations/2024-07-04-011736_registration_links/down.sql
@@ -1,3 +1,12 @@
 -- This file should undo anything in `up.sql`
 
 DROP TABLE IF EXISTS registration_links;
+
+DROP INDEX IF EXISTS idx_registration_links_user_id;
+DROP INDEX IF EXISTS idx_registration_links_project_id;
+DROP INDEX IF EXISTS idx_registration_links_group_id;
+DROP INDEX IF EXISTS idx_registration_links_class_id;
+DROP INDEX IF EXISTS idx_registration_links_used_by;
+
+
+
