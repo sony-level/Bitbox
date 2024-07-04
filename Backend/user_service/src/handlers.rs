695ee7ce6c55d44bd3e0cdf2241aa3591d30c125
sony-level <sony-dilane-level.mbango@isen.yncrea.fr>
2024-07-04 04:16:52 +0200

    feat : add function to serch user by id

--- Backend/user_service/src/handlers.rs
+++ Backend/user_service/src/handlers.rs
@@ -8,6 +8,7 @@ use diesel::prelude::*;
 use rocket::State;
 use sha2::{Sha512 , Digest};
 use chrono::Utc;
+use uuid::Uuid;
 use common::db::Pool;
 use rocket::http::Status;
 use rocket::response::status::{self, Custom};
@@ -16,7 +17,7 @@ use rocket::{
     serde::{json::Json, json::Value as JsonValue} ,
  };
 use rocket::serde::json::json;
-use domain::models::{User, NewUser , UserDisplay };
+use domain::models::{User, NewUser , UserDisplay , UpdateUser };
 
 fn hash_password(password: &str) -> String {
     let mut hasher = Sha512::new();
@@ -77,6 +78,54 @@ pub fn get_users(pool: &State<Pool>) -> Result<status::Custom<Json<Vec<UserDispl
             json!({ "error": err.to_string() })
         )),
     }
+}
+
+/**
+ * Récupérer un utilisateur par son identifiant
+ * @param id : l'identifiant de l'utilisateur
+ * @param pool : la connexion à la base de données
+ * @return l'utilisateur correspondant à l'identifiant
+ * @throws InternalServerError si la connexion à la base de données ne fonctionne pas
+ * @see establish_connection
+ * @see users
+ */
+#[rocket::get("/users/<id>")]
+pub fn get_users_by_id(id: Uuid, pool: &State<Pool>) -> Result<status::Custom<Json<UserDisplay>>, status::Custom<JsonValue>> {
+    let mut conn = pool.get().map_err(|_| Custom(Status::ServiceUnavailable, json!({"error": "Database connection error"})))?;
+    match users.find(id).first::<User>(&mut conn) {
+        Ok(user) => {
+            let user_display: UserDisplay= user.into();
+            Ok(Custom(Status::Ok, Json(user_display)))
+        },
+        Err(err) => Err(Custom(
+            Status::InternalServerError,
+            json!({ "error": err.to_string() })
+        )),
+    }
+}
+
+/**
+ * Mettre à jour un utilisateur
+ * @param id : l'identifiant de l'utilisateur
+ * @param user : les informations de l'utilisateur à mettre à jour
+ * @param pool : la connexion à la base de données
+ * @return l'utilisateur mis à jour
+ * @throws InternalServerError si la connexion à la base de données ne fonctionne pas
+ * @see establish_connection
+ * @see users
+ */
+#[rocket::put("/users/<id>", format = "application/json", data = "<user>")]
+pub fn update_user(id: Uuid, user: Json<UpdateUser>, pool: &State<Pool>) -> Result<status::Custom<Json<User>>, status::Custom<JsonValue>> {
+    let mut conn = pool.get().map_err(|_| Custom(Status::ServiceUnavailable, json!({"error": "Database connection error"})))?;
+    let user = user.into_inner();
+    let user = diesel::update(users.find(id))
+        .set(&user)
+        .get_result::<User>(&mut conn)
+        .map_err(|err| Custom(
+            Status::InternalServerError,
+            json!({ "error": err.to_string() })
+        ))?;
+    Ok(Custom(Status::Ok, Json(user)))
 }
 
 
