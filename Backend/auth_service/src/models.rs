782c420c6e177f82ddc05f0b59feb307fd555386
sony-level <sony-dilane-level.mbango@isen.yncrea.fr>
2024-07-06 17:55:55 +0200

    refactor: update data models

--- Backend/auth_service/src/models.rs
+++ Backend/auth_service/src/models.rs
@@ -1,39 +1,56 @@
 extern crate common;
 extern crate domain;
 
-use schemars::JsonSchema;
-use chrono::NaiveDateTime;
-use diesel::prelude::*;
-use diesel::sql_types::Uuid as DieselUuid;
+
+//use diesel::prelude::*;
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
-use domain::models::UserRole;
-//use domain::schema::UserRoles;
 
-#[derive(Deserialize, Serialize, JsonSchema)]
+use domain::models::UserRole;
+//use domain::schema::auth_tokens::user_id;
+
+/**
+ * Représente un utilisateur
+ */
+#[derive(Deserialize, Serialize,)]
 pub struct RegisterRequest {
     pub email: String,
     pub first_name: String,
     pub last_name: String,
     pub password: String,
+    pub totp_secret: String,
+    pub role: UserRole,
 }
 
-
-#[derive(Deserialize, Serialize, JsonSchema)]
+/**
+    * Représente une demande de connexion
+    */
+#[derive(Deserialize, Serialize)]
 pub struct LoginRequest {
     pub email: String,
-    pub password: String,
+    pub password:  Option<String>,
+    pub token: Option<String>
 }
 
-#[derive(Queryable, Identifiable, Insertable, AsChangeset, Debug, JsonSchema, Deserialize, Serialize, Deserialize)]
-pub struct Claims {
-    pub(crate) sub: Uuid,
-    pub(crate) exp: usize,
+/**
+    *
+    */
+#[derive(Deserialize, Serialize)]
+pub struct ClaimsType {
+    pub sub: Uuid,
+    pub exp: usize,
 }
 
-#[derive(Queryable, Identifiable, Insertable, AsChangeset, Debug, JsonSchema)]
+#[derive( Serialize, Deserialize)]
 pub struct AuthenticatedUser {
     pub id: Uuid,
-    pub role: UserRole
+    pub role: UserRole,
+    pub token: String,
+    pub totp_qr_code: Option<String>,
+}
+#[derive(Deserialize , Serialize)]
+pub struct Verify2FARequest {
+    pub user_id: Option<Uuid>,
+    pub totp_code: String,
 }
 
