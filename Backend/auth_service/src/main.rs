cc0ae89e6a4c3ca75cbd8c8d60391f647ea715db
sony-level <sony-dilane-level.mbango@isen.yncrea.fr>
2024-07-07 13:43:34 +0200

    refactor : remove titled dependencies

--- Backend/auth_service/src/main.rs
+++ Backend/auth_service/src/main.rs
@@ -6,20 +6,13 @@ mod routes;
 mod utils;
 mod models;
 
-//use common::db::Pool;
-//use common::db::establish_connection;
 
 
-use rocket::figment::Figment;
-use rocket::config::{Config};
+
 use rocket::{ Build, Rocket};
 use rocket::http::Method;
-//use rocket::form::FromForm;
-//use rocket::time::format_description::well_known::iso8601::Config;
 use rocket_cors::{AllowedOrigins, CorsOptions};
 use rocket_okapi::swagger_ui::{make_swagger_ui, SwaggerUIConfig};
-//use rocket_okapi::{openapi_get_routes};
-//use crate::handlers::register;
 
 /**
  * Lancer le serveur Rocket
@@ -30,8 +23,8 @@ use rocket_okapi::swagger_ui::{make_swagger_ui, SwaggerUIConfig};
 #[launch]
 fn rocket() -> Rocket<Build> {
 
-    let figment = Figment::from(Config::default())
-        .merge(("openapi.title", "Auth Service"));
+    //let figment = Figment::from(Config::default())
+        //.merge(("openapi.title", "Auth Service"));
 
         //.merge(("tls", TlsConfig::from_paths("/path/to/your/cert.pem", "/path/to/your/key.pem")));
 
@@ -45,24 +38,23 @@ fn rocket() -> Rocket<Build> {
 
     dotenv::dotenv().ok();
     let pool = common::db::establish_connection();
-    rocket::custom(figment)
+    rocket::build()
         .manage(pool)
+        .attach(cors)
         .mount(
             "/api/v1",
             routes![
-                handlers::register, //handlers::register,
+                handlers::register,
                 handlers::login,
                 handlers::verify_2fa,
                 //handlers::admin_route,
                 //handlers::get_user,
             ],
         )
-        .mount("/swagger", make_swagger_ui(&SwaggerUIConfig {
+      .mount("/swagger", make_swagger_ui(&SwaggerUIConfig {
             url: "/openapi.json".to_string(),
             ..Default::default()
         }))
-        .attach(cors)
-
 }
 
 
