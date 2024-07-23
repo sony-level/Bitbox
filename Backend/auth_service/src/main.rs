fecca1b91a3605187372f0c6da296a6249aeab2e
sony-level <sony-dilane-level.mbango@isen.yncrea.fr>
2024-07-23 15:24:09 +0200

    refactor: update  function to launch api service

--- Backend/auth_service/src/main.rs
+++ Backend/auth_service/src/main.rs
@@ -2,61 +2,75 @@
 extern crate diesel;
 
 mod handlers;
-//mod routes;
 mod utils;
 mod models;
+mod guard;
+use std::env;
+use rocket::fairing::AdHoc;
+use crate::models::AuthConfig;
+use rocket::{Build, Rocket};
+use crate::models::{ LoginRequest, LoginResponse, RegisterRequest};
+use domain::models::{Error, Response};
 
 
 
 
-use rocket::{ Build, Rocket};
-use rocket::http::Method;
-use rocket_cors::{AllowedOrigins, CorsOptions};
-//use rocket_okapi::swagger_ui::{make_swagger_ui, SwaggerUIConfig};
+use utoipa::OpenApi;
+
+
+#[derive(OpenApi)]
+#[openapi(
+    paths(
+        handlers::register,
+        handlers::login,
+        handlers::token_confirm,
+        handlers::logout,
+        handlers::protected_route
+    ),
+    components(
+        schemas(
+            RegisterRequest,
+            LoginRequest,
+            LoginResponse,
+        )
+    ),
+    tags(
+        (name = "Bitbox  API", description = "Bitbox API endpoints.")
+    )
+)]
+struct ApiDoc;
+
+
 
 /**
- * Lancer le serveur Rocket
- * @return le serveur Rocket
- * @throws InternalServerError si la connexion à la base de données ne fonctionne pas
- * @see establish_connection
- */
+* Lancer le serveur Rocket
+* @return le serveur Rocket
+* @throws InternalServerError si la connexion à la base de données ne fonctionne pas
+* @see establish_connection
+*/
 #[launch]
 fn rocket() -> Rocket<Build> {
-
-    //let figment = Figment::from(Config::default())
-        //.merge(("openapi.title", "Auth Service"));
-
-        //.merge(("tls", TlsConfig::from_paths("cert/cert.pem", "cert/key.pem")));
-
-    let cors = CorsOptions::default()
-        .allowed_origins(AllowedOrigins::all())
-        .allowed_methods(vec![Method::Get, Method::Post, Method::Put, Method::Delete].into_iter().map(From::from).collect())
-        //.allowed_headers(vec![rocket::http::Header::authorization(), rocket::http::Header::CONTENT_TYPE])
-        .allow_credentials(true)
-        .to_cors()
-        .expect("error creating CORS fairing");
-
     dotenv::dotenv().ok();
+    let jwt_secret = env::var("JWT_SECRET").expect("JWT_SECRET must be set");
     let pool = common::db::establish_connection();
     rocket::build()
         .manage(pool)
-        .attach(cors)
+        .manage(AuthConfig { jwt_secret })
         .mount(
             "/api/v1",
             routes![
                 handlers::register,
+                handlers::token_confirm,
                 handlers::login,
                 handlers::logout,
-                handlers::verify_2fa,
-                handlers::totp_setup,
-                handlers::reset_password,
-                handlers::forgot_password,
-
+                handlers::protected_route,
             ],
         )
+        .attach(AdHoc::try_on_ignite("OpenApi", |rocket| async {
+            Ok(rocket.manage(ApiDoc::openapi()))
+        }))
+
 
 }
 
 
-
-// Compare this snippet from Bitbox/Backend/auth_service/src/handlers.rs:
