50db6d3127ce4eae175ed1f8affa89ad1d282c3b
sony-level <sony-dilane-level.mbango@isen.yncrea.fr>
2024-07-23 21:16:55 +0200

    refactor: update  utils function to verifies passwor with argon2

--- Backend/auth_service/src/utils.rs
+++ Backend/auth_service/src/utils.rs
@@ -24,6 +24,7 @@ use lettre::transport::smtp::client::{Tls, TlsParameters};
 use native_tls::{TlsConnector, Protocol};
 use domain::models::{NewUser, User};
 use argon2::password_hash::Error as PasswordHashError;
+use config::Config;
 
 static GA_AUTH: OnceCell<GoogleAuthenticator> = OnceCell::new();
 
@@ -45,7 +46,14 @@ pub fn hash_password(password: &str) -> String {
     * @param hash : le mot de passe hashé
     * @return le mot de passe hashé
  */
-pub fn verify_password(password: &Option<String>, hash: &str) -> Result<bool, PasswordHashError> {
+pub fn verify_password(password: &str, hash: &str) -> bool {
+    let parsed_hash = PasswordHash::new(hash).unwrap();
+    let argon2 = Argon2::default();
+    argon2.verify_password(password.as_bytes(), &parsed_hash).is_ok()
+
+}
+
+pub fn verif_password(password: &Option<String>, hash: &str) -> Result<bool, PasswordHashError> {
     let argon2 = Argon2::default();
     let parsed_hash = PasswordHash::new(hash)?;
 
