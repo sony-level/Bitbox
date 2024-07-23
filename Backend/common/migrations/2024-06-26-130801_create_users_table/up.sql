471e2859bde141152759c3cc71a22b5beade54da
sony-level <sony-dilane-level.mbango@isen.yncrea.fr>
2024-07-23 15:21:21 +0200

    refactor: update line to pass in db

--- Backend/common/migrations/2024-06-26-130801_create_users_table/up.sql
+++ Backend/common/migrations/2024-06-26-130801_create_users_table/up.sql
@@ -6,8 +6,9 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(255) UNIQUE NOT NULL,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
-    totp_secret VARCHAR NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
+    totp_secret VARCHAR(255),
+    password VARCHAR(255) NOT NULL,
+    jwt_secret VARCHAR(255) NOT NULL,
     role user_role NOT NULL, -- 'student' or 'trainer'
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
