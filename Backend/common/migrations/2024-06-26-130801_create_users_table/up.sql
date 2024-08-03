c6b3e3266fa77ac238e82d978d0e9b90e64c6895
sony-level <sony-dilane-level.mbango@isen.yncrea.fr>
2024-08-03 14:22:30 +0200

    refactor : remove Mfa in table of users

--- Backend/common/migrations/2024-06-26-130801_create_users_table/up.sql
+++ Backend/common/migrations/2024-06-26-130801_create_users_table/up.sql
@@ -6,7 +6,6 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(255) UNIQUE NOT NULL,
     first_name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
-    totp_secret VARCHAR(255),
     password VARCHAR(255) NOT NULL,
     jwt_secret VARCHAR(255) NOT NULL,
     role user_role NOT NULL, -- 'student' or 'trainer'
