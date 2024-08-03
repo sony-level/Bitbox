f01cf89feb3bbd1f35a48cc2d0bb140739083037
sony-level <sony-dilane-level.mbango@isen.yncrea.fr>
2024-08-03 14:17:33 +0200

    feat add new feauture for request

--- Backend/auth_service/src/guard.rs
+++ Backend/auth_service/src/guard.rs
@@ -1,25 +1,27 @@
 extern crate diesel;
 extern crate dotenv;
 
-use diesel::pg::PgConnection;
-use diesel::r2d2::{self, ConnectionManager};
-use diesel::prelude::*;
-use rocket::outcome::Outcome;
-use rocket::http::{Cookie, CookieJar, Status};
-use rocket::request::{self, FromRequest, Request};
+use std::env;
+use chrono::Utc;
 use rocket::State;
+use diesel::prelude::*;
+use domain::models::{User, UserRole};
+use rocket::outcome::Outcome;
+use rocket::http::{ Status};
+use rocket::request::{self, FromRequest, Request};
 use uuid::Uuid;
-use domain::models::User;
-use domain::schema::users;
-use crate::models::ClaimsType;
-use crate::utils::{decode_jwt_no_secret, decode_token};
+use domain::schema::{auth_tokens, users};
+use rocket_sync_db_pools::Error;
+use common::db::Pool;
+use crate::models::AuthConfig;
+use crate::utils::{decode_jwt, decode_jwt_no_secret, decode_token};
 
-pub type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
+
 
 pub struct AuthenticatedUser {
     pub id: Uuid,
     pub email: String,
-    pub role: String,
+    pub role: domain::models::UserRole,
 }
 
 #[rocket::async_trait]
@@ -27,40 +29,46 @@ impl<'r> FromRequest<'r> for AuthenticatedUser {
     type Error = ();
 
     async fn from_request(request: &'r Request<'_>) -> request::Outcome<Self, Self::Error> {
-        let cookies = request.cookies();
-        let token = cookies.get_private("token").map(|cookie| cookie.value().to_string());
+        let token_cookie = request.cookies().get_private("token");
+        if let Some(cookie) = token_cookie {
+            let token = cookie.value();
+            let pool = request.rocket().state::<Pool>().unwrap();
+            let mut conn = pool.get().unwrap();
 
-        if let Some(token) = token {
-            let pool = request.guard::<&State<Pool>>().await.succeeded().unwrap();
-            let mut conn = match pool.get() {
-                Ok(conn) => conn,
-                Err(_) => return Outcome::Error((Status::ServiceUnavailable, ())),
-            };
 
-            let claims = match decode_jwt_no_secret(&token) {
+            let claims = match decode_jwt_no_secret(token) {
                 Ok(data) => data,
                 Err(_) => return Outcome::Error((Status::Unauthorized, ())),
             };
 
-            let user = match users::table
+            let user_result: Result<(String, UserRole, String), _> = users::table
                 .filter(users::id.eq(claims.sub))
-                .first::<User>(&mut conn) {
-                Ok(user) => user,
-                Err(_) => return Outcome::Error((Status::Unauthorized, ())),
-            };
+                .select((users::jwt_secret, users::role, users::email))
+                .first(&mut conn);
 
-            match decode_token(&token, &user.jwt_secret) {
-                Ok(data) => {
-                    // Retour de l'utilisateur authentifié
-                    Outcome::Success(AuthenticatedUser {
-                        id: user.id,
-                        email: user.email,
-                        role: user.role.to_string(),
-                    })
-                },
+            match user_result {
+                Ok((secret, role, email)) => {
+                    match decode_token(token, &secret) {
+                        Ok(token_data) => {
+                            let now = Utc::now().timestamp() as usize;
+                            if token_data.claims.exp < now {
+                                diesel::delete(auth_tokens::table.filter(auth_tokens::token.eq(token)))
+                                    .execute(&mut conn)
+                                    .expect("Error deleting expired token");
+
+                                return Outcome::Error((Status::Unauthorized, ()));
+                            }
+                            Outcome::Success(AuthenticatedUser {
+                                id: claims.sub,
+                                email,
+                                role,
+                            })
+                        }
+                        Err(_) => Outcome::Error((Status::Unauthorized, ())),
+                    }
+                }
                 Err(_) => Outcome::Error((Status::Unauthorized, ())),
             }
-
         } else {
             Outcome::Error((Status::Unauthorized, ()))
         }
