f6faf467ff29ff7823e17b96b5c919bb1b3f3b02
sony-level <sony-dilane-level.mbango@isen.yncrea.fr>
2024-08-03 14:17:43 +0200

    feat add new feauture for request

--- Backend/auth_service/src/handlers.rs
+++ Backend/auth_service/src/handlers.rs
@@ -3,9 +3,10 @@ extern crate domain;
 
 
 
+use crate::utils::{decode_token, is_password_strong};
 use rocket::response::status::{Custom};
 
-use crate::models::{AuthConfig};
+use crate::models::{AuthConfig, CreateStudentRequest};
 use domain::models::{User, NewUser, UserRole, NewAuthToken, Response, Error, EmailConfirmation, NewEmailConfirmation, AuthToken};
 use domain::schema::*;
 use diesel::prelude::*;
@@ -13,7 +14,7 @@ use rocket::http::{Cookie, CookieJar, Status};
 use common::db::Pool;
 use rocket::State;
 use crate::models::{ RegisterRequest, LoginRequest};
-use crate::utils::{hash_password, generate_token, verify_password,  send_confirmation_email, generat_token, generate_jwt, decode_jwt};
+use crate::utils::{hash_password, generate_token, verify_password, send_confirmation_email, generat_token, generate_jwt, decode_jwt};
 use chrono::Utc;
 use rocket::serde::json::{Json};
 use domain::schema::auth_tokens;
@@ -66,13 +67,23 @@ pub fn register(register_request: Json<RegisterRequest>, pool: &State<Pool>,  co
     if existing_user_count > 0 {
         return Err(Custom(Status::Conflict, Json(Error { error: "Un utilisateur existe déja avec cet Email".to_string() })));
     }
-
-
+    if !is_password_strong(&register_request.0.password) {
+        return Err(Custom(Status::BadRequest, Json(Error { error: "Password is not strong enough".to_string() })));
+    }
     let user_id = Uuid::new_v4();
     let email_confirmation_token = generate_jwt(user_id, &config.jwt_secret, 30);
 
     let hashed_password = hash_password(&register_request.0.password);
 
+
+
+    match send_confirmation_email(&register_request.0.email, &email_confirmation_token) {
+        Ok(_) => println!("Confirmation email sent"),
+        Err(e) => {
+            println!("Error sending confirmation email: {:?}", e);
+            return Err(Custom(Status::InternalServerError, Json(Error { error: "Internal server error".to_string() })));
+        }
+    }
     let new_email_confirmation = NewEmailConfirmation {
         id: user_id,
         email: &register_request.0.email,
@@ -83,20 +94,12 @@ pub fn register(register_request: Json<RegisterRequest>, pool: &State<Pool>,  co
         created_at: Some(Utc::now().naive_utc()),
         expires_at : Utc::now().naive_utc() + chrono::Duration::minutes(30),
     };
-
     match diesel::insert_into(email_confirmations::table)
         .values(&new_email_confirmation)
         .execute(&mut conn) {
         Ok(_) => println!("Email confirmation token inserted"),
         Err(e) => {
             println!("Error saving email confirmation token: {:?}", e);
-            return Err(Custom(Status::InternalServerError, Json(Error { error: "Internal server error".to_string() })));
-        }
-    }
-    match send_confirmation_email(&register_request.0.email, &email_confirmation_token) {
-        Ok(_) => println!("Confirmation email sent"),
-        Err(e) => {
-            println!("Error sending confirmation email: {:?}", e);
             return Err(Custom(Status::InternalServerError, Json(Error { error: "Internal server error".to_string() })));
         }
     }
@@ -134,16 +137,16 @@ pub fn token_confirm(token: String, pool: &State<Pool>, config: &State<AuthConfi
             return Err(Custom(Status::ServiceUnavailable, Json(Error { error: "Service unavailable".to_string() })));
         }
     };
-
-
-    let claims = match decode_jwt(&token, &config.jwt_secret) {
+    let claims = match decode_jwt(
+        &token,
+        &config.jwt_secret,
+    ) {
         Ok(data) => data.claims,
         Err(_) => {
             println!("Invalid or expired token");
             return Err(Custom(Status::Unauthorized, Json(Error { error: "Invalid or expired token".to_string() })));
         }
     };
-
     let email_confirmation = match email_confirmations::table
         .filter(email_confirmations::token.eq(&token))
         .select(EmailConfirmation::as_select())
@@ -162,17 +165,11 @@ pub fn token_confirm(token: String, pool: &State<Pool>, config: &State<AuthConfi
     let email = email_confirmation.email.clone();
     let first_name = email_confirmation.first_name.clone();
     let last_name = email_confirmation.last_name.clone();
-    let password = email_confirmation.password_hash.clone();
+    let pass = email_confirmation.password_hash.clone();
 
     diesel::delete(email_confirmations::table.filter(email_confirmations::id.eq(email_confirmation.id)))
         .execute(&mut conn)
         .expect("Error deleting email confirmation token");
-
-    let secret = thread_rng()
-        .sample_iter(&Alphanumeric)
-        .take(64)
-        .map(char::from)
-        .collect::<String>();
 
     let jwt_secret = thread_rng()
         .sample_iter(&Alphanumeric)
@@ -180,21 +177,17 @@ pub fn token_confirm(token: String, pool: &State<Pool>, config: &State<AuthConfi
         .map(char::from)
         .collect::<String>();
 
-    let hashed_password = hash_password(&password);
-
     let new_user = NewUser {
         id :  claims.sub,
         email: &email,
         first_name: &first_name,
         last_name: &last_name,
-        totp_secret: &secret,
-        password: &hashed_password,
+        password: &pass,
         jwt_secret: &jwt_secret,
         role: UserRole::Trainer,
         created_at: Some(Utc::now().naive_utc()),
         updated_at: Some(Utc::now().naive_utc()),
     };
-
     let user_id = match diesel::insert_into(users::table)
         .values(&new_user)
         .returning(users::id)
@@ -205,9 +198,7 @@ pub fn token_confirm(token: String, pool: &State<Pool>, config: &State<AuthConfi
             return Err(Custom(Status::InternalServerError, Json(Error { error: "Internal server error".to_string() })));
         }
     };
-
     let auth_token = generate_token(&new_user);
-
     let new_auth_token = NewAuthToken {
         user_id: Some(user_id),
         token: &auth_token,
@@ -224,7 +215,6 @@ pub fn token_confirm(token: String, pool: &State<Pool>, config: &State<AuthConfi
             return Err(Custom(Status::InternalServerError, Json(Error { error: "Internal server error".to_string() })));
         }
     }
-
     Ok(Json(Response { message: "User confirmed.".to_string() }))
 }
 
@@ -250,64 +240,50 @@ pub fn token_confirm(token: String, pool: &State<Pool>, config: &State<AuthConfi
     )
 )]
 #[post("/login", format = "application/json", data = "<login_request>")]
-pub fn login(login_request: Json<LoginRequest>, pool: &State<Pool>, cookies: &CookieJar<'_>) -> Result<Json<Response>, Custom<Json<Error>>> {
+pub fn login(login_request: Json<LoginRequest>, pool: &State<Pool>, cookies: &CookieJar<'_> , config: &State<AuthConfig>) -> Result<Json<Response>, Custom<Json<Error>>> {
     let mut conn = pool.get().map_err(|_| Custom(Status::ServiceUnavailable, Json(Error { error: "Service Unavailable".to_string() })))?;
 
-    // Recherche de l'utilisateur par email
     let user = users::table
         .filter(users::email.eq(&login_request.email))
         .first::<User>(&mut conn)
         .map_err(|_| Custom(Status::Unauthorized, Json(Error { error: "Invalid credentials".to_string() })))?;
 
-    // Vérification des anciens tokens
-    let auth_token_result = auth_tokens::table
-        .filter(auth_tokens::user_id.eq(user.id))
-        .filter(auth_tokens::expires_at.gt(Utc::now().naive_utc()))
-        .first::<AuthToken>(&mut conn)
-        .optional()
-        .map_err(|_| Custom(Status::InternalServerError, Json(Error { error: "Internal server error".to_string() })))?;
-
-    if let Some(auth_token) = auth_token_result {
-        println!("Token valide trouvé pour l'utilisateur, connexion réussie.");
-        let mut cookie = Cookie::new("token", auth_token.token.clone());
-        cookie.set_http_only(true);
-        cookie.set_secure(true);
-        cookie.set_max_age(time::Duration::hours(24));
-        cookies.add_private(cookie);
-
-        return Ok(Json(Response { message: "Login successful".to_string() }));
-    } else {
-
-        diesel::delete(auth_tokens::table.filter(auth_tokens::user_id.eq(user.id)))
-            .execute(&mut conn)
-            .map_err(|_| Custom(Status::InternalServerError, Json(Error { error: "Internal server error".to_string() })))?;
-
-        if verify_password(&login_request.password, &user.password) {
-            let token =  generat_token(&user);
-            let new_auth_token = NewAuthToken {
-                user_id: Some(user.id),
-                token: &token,
-                created_at: Some(Utc::now().naive_utc()),
-                expires_at: Utc::now().naive_utc() + chrono::Duration::hours(24),
-            };
-
-            diesel::insert_into(auth_tokens::table)
-                .values(&new_auth_token)
-                .execute(&mut conn)
-                .map_err(|_| Custom(Status::InternalServerError, Json(Error { error: "Internal server error".to_string() })))?;
-
-            let mut cookie = Cookie::new("token", token);
-            cookie.set_http_only(true);
-            cookie.set_secure(true);
-            cookie.set_max_age(time::Duration::hours(24));
-            cookies.add_private(cookie);
-
-            Ok(Json(Response { message: "Login successful".to_string() }))
-        } else {
-            println!("Mot de passe incorrect");
-            Err(Custom(Status::Unauthorized, Json(Error { error: "Invalid credentials".to_string() })))
+    match verify_password(&login_request.password, &user.password) {
+        Ok(is_valid) => {
+            if !is_valid {
+                return Err(Custom(Status::Unauthorized, Json(Error { error: "Invalid credentials for password".to_string() })));
+            }
+        }
+        Err(_) => {
+            return Err(Custom(Status::Unauthorized, Json(Error { error: "Invalid credentials for login".to_string() })));
         }
     }
+
+    let token = generat_token(&user);
+    diesel::delete(auth_tokens::table.filter(auth_tokens::user_id.eq(user.id)))
+        .execute(&mut conn)
+        .map_err(|_| Custom(Status::InternalServerError, Json(Error { error: "Internal server error".to_string() })))?;
+
+    let new_auth_token = NewAuthToken {
+        user_id: Some(user.id),
+        token: &token,
+        created_at: Some(Utc::now().naive_utc()),
+        expires_at: Utc::now().naive_utc() + chrono::Duration::hours(24),
+    };
+
+    diesel::insert_into(auth_tokens::table)
+        .values(&new_auth_token)
+        .execute(&mut conn)
+        .map_err(|_| Custom(Status::InternalServerError, Json(Error { error: "Internal server error".to_string() })))?;
+
+    let mut cookie = Cookie::new("token", token);
+    cookie.set_http_only(true);
+    cookie.set_secure(true);
+    cookie.set_max_age(time::Duration::hours(24));
+    cookies.add_private(cookie);
+
+    Ok(Json(Response { message: "Login successful".to_string() }))
+
 }
 
 
@@ -336,15 +312,25 @@ pub fn logout(cookies: &CookieJar<'_>, pool: &State<Pool>, user: AuthenticatedUs
         let token_value = token_cookie.value().to_string();
         let mut conn = pool.get().map_err(|_| Custom(Status::ServiceUnavailable, Json(Error { error: "Service Unavailable".to_string() })))?;
 
-        diesel::delete(auth_tokens::table.filter(auth_tokens::token.eq(&token_value)))
-            .execute(&mut conn)
+        let auth_token_result = auth_tokens::table
+            .filter(auth_tokens::token.eq(&token_value))
+            .filter(auth_tokens::user_id.eq(user.id))
+            .first::<AuthToken>(&mut conn)
+            .optional()
             .map_err(|_| Custom(Status::InternalServerError, Json(Error { error: "Internal server error".to_string() })))?;
 
-        cookies.remove_private(Cookie::build("token"));
+        if let Some(_) = auth_token_result {
+            diesel::delete(auth_tokens::table.filter(auth_tokens::token.eq(&token_value)))
+                .execute(&mut conn)
+                .map_err(|_| Custom(Status::InternalServerError, Json(Error { error: "Internal server error".to_string() })))?;
+
+            cookies.remove_private(Cookie::build("token"));
+        }
     }
 
     Ok(Json(Response { message: "User logged out".to_string() }))
 }
+
 
 
 /**
@@ -367,4 +353,83 @@ pub fn logout(cookies: &CookieJar<'_>, pool: &State<Pool>, user: AuthenticatedUs
 #[get("/protected")]
 pub fn protected_route(user: AuthenticatedUser) -> String {
     format!("Welcome, {}. Your role is {}.", user.email, user.role)
+}
+
+
+/**
+    * le trainer creeajoute un student
+    * @param pool : la connexion à la base de données
+    * @param user : l'utilisateur connecté
+    * @param student : le student à ajouter
+    * @return le student ajoute
+    * @throws Unauthorized si l'utilisateur n'est pas connecté
+    * @throws NotFound si le trainer n'existe pas
+    * @throws InternalServerError si la connexion à la base de données ne fonctionne pas
+    * @see AuthenticatedUser
+    * $ curl -X POST http://localhost:8000/api/v1/add_student -H "Content-Type: application/json" -d '{"email": "test", "first_name": "test", "last_name": "test"}' -H "Cookie: token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ"
+    */
+
+#[utoipa::path(
+    post,
+    path = "/students/create",
+    tag = "User",
+    request_body(content = CreateStudentRequest),
+    responses(
+        (status = 201, description = "Student created successfully", body = Response),
+        (status = 400, description = "Invalid input", body = Error),
+        (status = 500, description = "Internal server error", body = Error),
+    )
+)]
+#[post("/students/create", format = "application/json", data = "<student_request>")]
+pub fn create_student(student_request: Json<CreateStudentRequest>, pool: &State<Pool>, user: AuthenticatedUser) -> Result<Json<Response>, Custom<Json<Error>>> {
+    if user.role != UserRole::Trainer {
+        return Err(Custom(Status::Unauthorized, Json(Error { error: "Only trainers can create students.".to_string() })));
+    }
+
+    if student_request.0.email.is_empty() || student_request.0.first_name.is_empty() || student_request.0.last_name.is_empty() {
+        return Err(Custom(Status::BadRequest, Json(Error { error: "Invalid input".to_string() })))
+    }
+
+    let mut conn = pool.get().map_err(|_| Custom(Status::ServiceUnavailable, Json(Error { error: "Service unavailable".to_string() })))?;
+    let existing_user_count = users::table.filter(users::email.eq(&student_request.0.email)).count().get_result::<i64>(&mut conn).expect("Error counting users");
+    if existing_user_count > 0 {
+        return Err(Custom(Status::Conflict, Json(Error { error: "Un utilisateur existe déjà avec cet Email".to_string() })));
+    }
+
+    let user_id = Uuid::new_v4();
+    let jwt_secret = thread_rng().sample_iter(&Alphanumeric).take(64).map(char::from).collect::<String>();
+    let new_student = NewUser {
+        id: user_id,
+        email: &student_request.0.email,
+        first_name: &student_request.0.first_name,
+        last_name: &student_request.0.last_name,
+        password: "",
+        jwt_secret: &jwt_secret,
+        role: UserRole::Student,
+        created_at: Some(Utc::now().naive_utc()),
+        updated_at: Some(Utc::now().naive_utc()),
+    };
+
+    diesel::insert_into(users::table).values(&new_student).execute(&mut conn).map_err(|e| {
+        println!("Error saving new student: {:?}", e);
+        Custom(Status::InternalServerError, Json(Error { error: "Internal server error".to_string() }))
+    })?;
+
+    Ok(Json(Response { message: "Student created successfully.".to_string() }))
+}
+
+
+#[get("/auth/student_login?<token>")]
+pub fn student_login(token: String, pool: &State<Pool>, config: &State<AuthConfig>) -> Result<Json<Response>, Custom<Json<Error>>> {
+    let mut conn = pool.get().map_err(|_| Custom(Status::ServiceUnavailable, Json(Error { error: "Service unavailable".to_string() })))?;
+
+    let claims = decode_token(&token, &config.jwt_secret).map_err(|_| Custom(Status::Unauthorized, Json(Error { error: "Invalid or expired token".to_string() })))?;
+
+    let student = users::table
+        .filter(users::id.eq(claims.claims.sub))
+        .filter(users::role.eq(UserRole::Student))
+        .first::<User>(&mut conn)
+        .map_err(|_| Custom(Status::Unauthorized, Json(Error { error: "Invalid or expired token".to_string() })))?;
+
+    Ok(Json(Response { message: format!("Student {} authenticated.", student.email) }))
 }
